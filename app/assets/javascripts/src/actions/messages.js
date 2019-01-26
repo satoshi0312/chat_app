5abe906c1bfaa2e973b2ccccc2ea09aadecb6f28
関川智 <t.g.i.f.0312@gmail.com>
2019-01-26 17:50:26 +0900

    actions/messages.jsの編集とAPI,crsf対策の定数化

--- app/assets/javascripts/src/actions/messages.js
+++ app/assets/javascripts/src/actions/messages.js
@@ -1,19 +1,51 @@
+import request from 'superagent'
 import Dispatcher from '../dispatcher'
-import {ActionTypes} from '../constants/app' // 追記
+import {ActionTypes, APIEndpoints, CSRFToken} from '../constants/app'
 
 export default {
   changeOpenChat(newUserID) {
-    Dispatcher.handleViewAction({
-      type: ActionTypes.UPDATE_OPEN_CHAT_ID,
-      userID: newUserID,
+    return new Promise((resolve, reject) => {
+      request
+      .get(APIEndpoints.MESSAGES) // 取得したいjsonがあるURLを指定する
+      .end(error, res) => {
+        if (!error && res.status === 200) { // 200はアクセスが成功した際のステータスコードです。
+          const json = JSON.parse(res.text)
+          Dispatcher.handleServerAction({
+            type: ActionTypes.UPDATE_OPEN_CHAT_ID,
+            userID: newUserID,
+            json, // json: jsonと同じ。keyとvalueが一致する場合、このように省略出来ます。
+          })
+          resolve(json)
+        } else {
+          reject(res)
+        }
+      }
     })
   },
+
   sendMessage(userID, message) {
-    Dispatcher.handleViewAction({
-      type: ActionTypes.SEND_MESSAGE,
-      userID: userID,
-      message: message,
-      timestamp: +new Date(),
+    return new Promise((resolve, reject) => {
+      request
+      .post(APIEndpoints.MESSAGES) // 後ほど説明します。
+      .set('X-CSRF-Token', CSRFToken()) // 後ほど説明します。
+      // これによりサーバ側に送りたいデータを送ることが出来ます。
+      .send({
+        user_id: userId,
+        text: message,
+      })
+      .end((error, res) => {
+        if (!error && res.status === 200) {
+          const json = JSON.parse(res.text)
+          Dispatcher.handleServerAction({
+            type: ActionTypes.SEND_MESSAGE,
+            userId,
+            json,
+          })
+        } else {
+          reject(res)
+        }
+      })
     })
   },
+
 }
--- app/assets/javascripts/src/constants/app.js
+++ app/assets/javascripts/src/constants/app.js
@@ -4,3 +4,15 @@ export const ActionTypes = keyMirror({
   UPDATE_OPEN_CHAT_ID: null,
   SEND_MESSAGE: null,
 })
+
+//CSRFを防ぐためのセキュリティ
+export function CSRFToken() {
+  return document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+}
+
+// apiの共通のurlを定数化
+const Root = window.location.origin || `${window.location.protocol}//${window.location.hostname}`
+const APIRoot = `${Root}/api`
+export const APIEndpoints = {
+  MESSEGES: APIRoot + '/messages'
+}

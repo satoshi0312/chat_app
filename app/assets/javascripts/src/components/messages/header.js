2925501fee1f0b29d6f8434e647c463aeed5c551
関川智 <t.g.i.f.0312@gmail.com>
2019-01-31 23:41:41 +0900

    HEADERコンポーネント編集とユーザ詳細画面のHTML追加

--- app/assets/javascripts/src/components/messages/header.js
+++ app/assets/javascripts/src/components/messages/header.js
@@ -4,6 +4,32 @@ class Header extends React.Component {
   render() {
     return (
         <header className='header'>
+          <div className='header-left'>
+            <div className='header-logo'>
+              <a href='/'>ChatApp</a>
+            </div>
+          </div>
+          <div className='header-right'>
+            <ul className='nav nav-tabs'>
+              <li className='search-user-btn'>
+                <a href='ユーザーを探す' className='header-logo-link'>/users/search</a>
+              </li>
+              <li className='dropdown'>
+                <div>
+                  test
+                  <span></span>
+                </div>
+                <ul className='dropdown-menu user-menu-box'>
+                  <li className='user-menu-list'>
+                    <a href='マイページ'>#</a>
+                  </li>
+                  <li className='user-menu-list'>
+                    <a href='ログアウト'>#</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </header>
       )
   }

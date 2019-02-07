327d85f9862097648c929969c015928b952d038a
関川智 <t.g.i.f.0312@gmail.com>
2019-02-07 23:16:20 +0900

    ファイル名変更とusersStoreの作成

---
+++ app/assets/javascripts/src/stores/users.js
@@ -0,0 +1,38 @@
+import Dispatcher from '../dispatcher'
+import BaseStore from '../base/store'
+import {ActionTypes} from '../constants/app'
+
+class UsersStore extends BaseStore {
+  getUsers() {
+    if (!this.get('searhUsers')) this.setUsers([])
+    return this.get('searhUsers')
+  }
+  setUsers(array) {
+    this.set('searhUsers', array)
+  }
+  addChangeListener(callback) {
+    this.on('change', callback)
+  }
+  removeChangeListener(callback) {
+    this.off('change', callback)
+  }
+}
+const UserStore = new UsersStore()
+
+UsersStore.dispatchToken = Dispatcher.register(payload => {
+  const action = payload.action
+
+  switch (action.type) {
+    case ActionTypes.SEARCH_USERS:
+      console.log('UserStore : SEARCH_USERS')
+      console.log(action)
+      UserStore.setUsers(action.json)
+      UserStore.emitChange()
+      break
+
+  }
+
+  return true
+})
+
+export default UserStore

49ee532933f969ec7f434aa16f6c680258e9ba50
関川智 <t.g.i.f.0312@gmail.com>
2019-02-10 23:36:28 +0900

    ヘッダーコンポーネント編集

--- app/assets/javascripts/src/components/messages/header.js
+++ app/assets/javascripts/src/components/messages/header.js
@@ -1,7 +1,35 @@
 import React from 'react'
+import UsersStore from '../../stores/users'
+import UsersAction from '../../actions/users'
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = this.initialState
+    UsersAction.getCurrentUser()
+    this.onChangeHandler = this.onStoreChange.bind(this)
+  }
+  get initialState() {
+    return this.getStateFromStore()
+  }
+  getStateFromStore() {
+    return {
+      currentUser: UsersStore.getCurrentUser(),
+    }
+  }
+  componentWillMount() {
+    UsersStore.onChange(this.onStoreChange.bind(this))
+  }
+  componentWillUnmount() {
+    UsersStore.offChange(this.onStoreChange.bind(this))
+  }
+  onStoreChange() {
+    this.setState(this.getStateFromStore())
+  }
   render() {
+    const currentUser = this.state.currentUser
+    const currentUserName = currentUser ? currentUser.name : ''
+    const currentUserPage = currentUser ? (<a href={'/users/' + currentUser.id}>マイページ</a>) : ''
     return (
         <header className='header'>
           <div className='header-left'>
@@ -12,19 +40,19 @@ class Header extends React.Component {
           <div className='header-right'>
             <ul className='nav nav-tabs'>
               <li className='search-user-btn'>
-                <a href='ユーザーを探す' className='header-logo-link'>/users/search</a>
+                <a href='/users/search' className='header-logo-link'>ユーザーを探す</a>
               </li>
               <li className='dropdown'>
                 <div>
-                  test
+                  {currentUserName}
                   <span></span>
                 </div>
                 <ul className='dropdown-menu user-menu-box'>
                   <li className='user-menu-list'>
-                    <a href='マイページ'>#</a>
+                    {currentUserPage}
                   </li>
                   <li className='user-menu-list'>
-                    <a href='ログアウト'>#</a>
+                    <a href='/users/sign_out' data-method='delete'>ログアウト</a>
                   </li>
                 </ul>
               </li>
